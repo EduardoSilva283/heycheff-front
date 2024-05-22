6a7621633543008d4a3b62f87a18303aee2274b3
eduardo.silva283 <eduardo.silva283@fatec.sp.gov.br>
2024-05-22 13:30:05 -0300

    Adicionado Toast na hora de Salvar a Receita e desabilita o Botão Salvar receita

--- src/components/modal/modalCadReceita.js
+++ src/components/modal/modalCadReceita.js
@@ -1,6 +1,5 @@
 import Modal from 'react-bootstrap/Modal';
-import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import { Form, Button, Toast, ToastContainer } from 'react-bootstrap';
 import { useModal } from './ModalContext';
 import { useEffect, useState } from 'react';
 import { API_URL } from '../../constants/const';
@@ -13,6 +12,8 @@ function ModalCadReceita() {
     const [titulo, setTitulo] = useState('');
     const [selectedCategorias, setSelectedCategorias] = useState([]);
     const [file, setFile] = useState(null);
+    const [showToast, setShowToast] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     useEffect(() => {
         const fetchAll = async () => {
@@ -56,6 +57,7 @@ function ModalCadReceita() {
         formData.append('tags', JSON.stringify(selectedCategorias));
         formData.append('thumb', file);
 
+
         try {
             const response = await axios.post(API_URL + '/receitas', formData, {
                 headers: {
@@ -63,6 +65,8 @@ function ModalCadReceita() {
                 },
             });
             console.log('Success:', response.data);
+            setShowToast(true); // Exibir o Toast de sucesso
+            setIsSubmitting(true);
             //closeModal(); // Fecha o modal após o sucesso
         } catch (error) {
             console.error('Error:', error);
@@ -70,45 +74,57 @@ function ModalCadReceita() {
     };
 
     return (
-        <Modal show={isModalOpen} onHide={closeModal} fullscreen={true}>
-            <Modal.Header closeButton>
-                <Modal.Title>Cadastrar Receita</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formTitulo">
-                        <Form.Label>Título da Receita</Form.Label>
-                        <Form.Control
-                            type="text"
-                            value={titulo}
-                            onChange={(e) => setTitulo(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formCategorias">
-                        <Form.Label>Escolha as Categorias:</Form.Label>
-                        {categorias.map((c) => (
-                            <Form.Check
-                                key={c.id}
-                                inline
-                                label={c.tag}
-                                value={c.tag}
-                                name="categorias"
-                                type="checkbox"
-                                onChange={(e) => handleCategoriaChange(e, c)}
+        <>
+            <Modal show={isModalOpen} onHide={closeModal} fullscreen={true}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Cadastrar Receita</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formTitulo">
+                            <Form.Label>Título da Receita</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={titulo}
+                                onChange={(e) => setTitulo(e.target.value)}
                             />
-                        ))}
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formThumb">
-                        <Form.Label>Selecione a Thumb</Form.Label>
-                        <Form.Control type="file" onChange={handleFileChange} />
-                    </Form.Group>
-                    <Button variant="warning" type="submit">
-                        Salvar Receita
-                    </Button>
-                </Form>
-            </Modal.Body>
-        </Modal>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formCategorias">
+                            <Form.Label>Escolha as Categorias:</Form.Label>
+                            {categorias.map((c) => (
+                                <Form.Check
+                                    key={c.id}
+                                    inline
+                                    label={c.tag}
+                                    value={c.tag}
+                                    name="categorias"
+                                    type="checkbox"
+                                    onChange={(e) => handleCategoriaChange(e, c)}
+                                />
+                            ))}
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formThumb">
+                            <Form.Label>Selecione a Thumb</Form.Label>
+                            <Form.Control type="file" onChange={handleFileChange} />
+                        </Form.Group>
+                        <Button variant="warning" type="submit" disabled={isSubmitting}>
+                            {isSubmitting ? 'Receita Salva' : 'Salvar Receita'}
+                        </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
 
-    )
+            <ToastContainer position="top-end" className="p-3">
+                <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
+                    <Toast.Header>
+                        <strong className="me-auto">Sucesso</strong>
+                    </Toast.Header>
+                    <Toast.Body>Receita cadastrada com sucesso!</Toast.Body>
+                </Toast>
+            </ToastContainer>
+        </>
+
+
+    );
 }
 export default ModalCadReceita;
