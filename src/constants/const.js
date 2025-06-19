ce57cdeda8fa5575bc2e6d1dac84ddc0da2491dc
Eduardo Luis <eduardo.luis@linx.com.br>
2025-06-19 15:58:10 -0300

    altera a const.js para o URL_MEDIA local

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,8 +1,8 @@
 const URL_DEV = "http://localhost:6015/heycheff";
-const URL_MEDIA_DEV = "http://localhost:6015";
+const URL_MEDIA_DEV = "http://localhost:6015/heycheff/media?path=";
 
-const URL_PROD = "https://heycheff-api-h5bcceg3c8acgraz.westus-01.azurewebsites.net/heycheff";
-const URL_MEDIA_PROD = "https://heycheff-api-h5bcceg3c8acgraz.westus-01.azurewebsites.net/media?path=";
+const URL_PROD = "http://localhost:6015/heycheff";
+const URL_MEDIA_PROD = "http://localhost:6015/heycheff/media?path=";
 
 const isDev = process.env.NODE_ENV === 'development';
 
